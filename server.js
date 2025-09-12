110c000ae7c3a60e88c73ee363ca889baa0c17b7
d00055a <danieldedja2@gmail.com>
2025-09-12 02:32:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CORS_ORIGIN,
+  origin: 'https://d00055a.github.io',
   credentials: true,
 }));
 
@@ -30,3 +30,4 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
